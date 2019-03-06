2419f682063cd2bcff0eca91c7008a2d13d69512
Robert Reinert <rreinert2014@gmail.com>
2019-03-05 23:08:38 -0500

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # js_search
 Proof of concept for LAMP application, written in JavaScript (ES6)
+
+## Technologies
+- JavaScript ES6
+- HTML5
+- Bootstrap 4
+- jQuery 3.3
+- PhantomJS (http://phantomjs.org/)
--- search.js
+++ search.js
@@ -9,9 +9,7 @@ class SearchBot {
 		this.query = query.trim();
 		this.interval = interval;
 		
-		alert("Query: " + this.query);
-		alert("Number: " + this.num);
-		alert("Interval: " + this.interval);
+		alert("Query: " + this.query);		
 	}
 	
 	// Check the hash map for existing queries
@@ -19,22 +17,40 @@ class SearchBot {
 		//Example query
 		//"https://www.bing.com/search?q=how+to+play+cricket+ball"
 		var search_query; 
+		var page_html;
+		var start_index, end_index;
 		
 		if(this.array.length > 0) {
-			search_query = this.array.pop();
-			search_query.split(" ").join("+");
+			search_query = this.array.pop().split(" ").join("+");
 		} else {
-			search_query = this.query;
+			search_query = this.query.split(" ").join("+");;
+		}
+		
+		alert("New query: " + search_query);
+		
+		var iframe = document.getElementById("browser");		
+		iframe.src="https://www.bing.com/search?q=" + search_query
+		alert(iframe.src);
+		
+		// Scrape the page for related searches
+		if(this.array.length < this.num) {
+			//this.scrape_page(iframe);
+			page_html = iframe.contentWindow.document.body.innerHTML;
+			start_index = page_html.indexOf("Related searches");
+			end_index = page_html.indexOf("Pagination");
+			
+			page_html = page_html.substr(start_index,end_index);
+			
+			alert("Scraper: "+page_html);
+			
 		}
 		
 		
-		var iframe = document.getElementById("browser");		
-		alert(iframe.src);
 		
-		//iframe.src="https://www.bing.com/search?q=" + search_query
-		//iframe.reload();
-		
-		
+	}
+	scrape_page(frame_window) {
+	
+		page_html = frame_window.contentWindow.document.body.innerHTML;
 	}
 	
 	
