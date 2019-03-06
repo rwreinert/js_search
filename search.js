9267ea9e1483e881180044151f5a9ba930a8cd49
Robert Reinert <rreinert2014@gmail.com>
2019-03-05 22:48:32 -0500

    Add jQuery

--- search.js
+++ search.js
@@ -4,17 +4,36 @@ class SearchBot {
 	
 	constructor(query, num, interval) {
 		
-		this.array = new Array(num);
-		this.query = query;
+		this.array = new Array();
+		this.num = num;
+		this.query = query.trim();
 		this.interval = interval;
+		
+		alert("Query: " + this.query);
+		alert("Number: " + this.num);
+		alert("Interval: " + this.interval);
 	}
 	
 	// Check the hash map for existing queries
 	search() {
-		var search_query = this.array.pop();
+		//Example query
+		//"https://www.bing.com/search?q=how+to+play+cricket+ball"
+		var search_query; 
+		
+		if(this.array.length > 0) {
+			search_query = this.array.pop();
+			search_query.split(" ").join("+");
+		} else {
+			search_query = this.query;
+		}
+		
 		
 		var iframe = document.getElementById("browser");		
 		alert(iframe.src);
+		
+		//iframe.src="https://www.bing.com/search?q=" + search_query
+		//iframe.reload();
+		
 		
 	}
 	
